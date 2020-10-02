e897f8dba984eeb1aaaf8199fa518e06579cd04a
Caio Oliveira <colive.dev@gmail.com>
2020-10-02 12:05:00 -0300

    fixes type of redirect_url constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,3 @@
 export const CLIENT_ID = 'cd55801a38d74dffbeabab42479e8dac'
 export const CLIENT_SECRET = '43667da584864a6297d007a24c5d5749'
-export const REDIRECT_URI = process.env.host
+export const REDIRECT_URI = process.env.host || 'http://localhost:3000'
